f931846708cdf3b24285fc0bd48a87299338670e
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2021-09-20 21:47:25 +0300

    refactor: change video slider

--- museum/js/main.js
+++ museum/js/main.js
@@ -2,5 +2,5 @@ const progress = document.querySelectorAll('.control-input');
   
 progress.forEach(item =>item.addEventListener('input', function() {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #C4C4C4 ${value}%, #C4C4C4)`
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4)`
 }))
