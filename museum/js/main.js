65c3d085fd5b5ad2edab20de0adca83e917eadb4
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2021-09-21 16:07:19 +0300

    fix: random gallery generation. Change buy section

--- museum/js/main.js
+++ museum/js/main.js
@@ -4,3 +4,30 @@ progress.forEach(item =>item.addEventListener('input', function() {
   const value = this.value;
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4)`
 }))
+
+/* Gallery */
+const pictureInnerContainer = document.querySelector('.picture-inner-container');
+
+let imgCount = 15;
+let imgArr = [...Array(+`${imgCount}`).keys()].map(item => item +1)
+
+console.log(imgArr)
+
+const shuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
+
+shuffle(imgArr)
+
+imgArr.map(item => {
+  const img = document.createElement('img');
+  //img.classList.add('gallery-img')
+  img.src = `./assets/img/gallery/galery${item}.jpg`;
+  img.alt = `galery${item}`;
+  pictureInnerContainer.append(img);
+})
+
+console.log(imgArr)
