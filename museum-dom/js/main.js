67bca2e1fb18897da5ec099b70d00cdebf8bb2cf
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2021-10-11 12:41:25 +0300

    feat: fix form bug using localstorage

--- museum-dom/js/main.js
+++ museum-dom/js/main.js
@@ -568,56 +568,66 @@ const calcTotalPrice = () => {
   const basicTicketsTotalPrice = document.querySelector('.basik-ticket-total');
   const seniorTicketsTotalPrice = document.querySelector('.senior-ticket-total');
   const ticketsTypeSelect = document.querySelector('.tickets-type-select'); 
-
-  let options = document.querySelectorAll('.tickets-type-select option');
-
+  const payInfo = document.querySelector('.pay-info'); 
   const totalPrice = document.querySelectorAll('.price');
-  const permanentTicetPrice = 20;
   
   let totalTicketsPrice = 0;
   let totalBasicTicketsPrice = 0;
   let totalSeniorTicketsPrice = 0;
-
-  if (localStorage.getItem('selectedTicketType') === null) localStorage.setItem('selectedTicketType', permanentTicetPrice);
   
   basickTicketsAmount.value = localStorage.getItem('basickTicketsAmount') || 1;
   seniorTicketAmount.value = localStorage.getItem('seniorTicketAmount') || 1;
   formBasickTicketsAmount.value = localStorage.getItem('basickTicketsAmount') || 1;
   formSeniorTicketAmount.value = localStorage.getItem('seniorTicketAmount') || 1;
+  payInfo.textContent = localStorage.getItem('selectedTicketType') || 'Permanent exhibition';
 
   function selectTicketType() {
     localStorage.setItem('selectedTicketType', this.value);
-    localStorage.setItem('selectedTicketTypeName', this.id);
+    payInfo.textContent = localStorage.getItem('selectedTicketType');
     recalcPrice();
   } 
   
-  const setTicketType = () => {
-    options.forEach(currentItem => {
-      if (currentItem.value === localStorage.getItem('selectedTicketTypeName')) {
-        currentItem.selected = true;
-      }
-    })
-    console.log(1);
+  const ticketPrise = () => {
+    switch (localStorage.getItem('selectedTicketType')) {
+      case 'Permanent exhibition' : 
+        localStorage.setItem('ticketPrice', 20);
+        ticketsTypeSelect.selectedIndex = 1;
+        break;
+      case 'Temporary exhibition':  
+        localStorage.setItem('ticketPrice', 25);
+        ticketsTypeSelect.selectedIndex = 2;
+        break;
+      case 'Combined Admission':
+        localStorage.setItem('ticketPrice', 40);
+        ticketsTypeSelect.selectedIndex = 3;
+        break;
+    }
   }
 
-    ticketsTypeSelect.addEventListener('change', setTicketType);
-
-  ticetTypes.forEach(item => {
-    item.onchange = selectTicketType;
-    if (localStorage.getItem('selectedTicketType') === item.value) item.checked = true;
-  });
-
+  const changeTicketTypeSelect = () => {
+    ticetTypes.forEach(item => {
+      item.onchange = selectTicketType;
+      if (localStorage.getItem('selectedTicketType') === item.value) item.checked = true;
+    });
+  }
  
-
   const recalcPrice = () => {
-    totalBasicTicketsPrice = localStorage.getItem('basickTicketsAmount') * localStorage.getItem('selectedTicketType');
-    totalSeniorTicketsPrice = localStorage.getItem('seniorTicketAmount') * (localStorage.getItem('selectedTicketType') / 2);
+    ticketPrise();
+    totalBasicTicketsPrice = localStorage.getItem('basickTicketsAmount') * localStorage.getItem('ticketPrice');
+    totalSeniorTicketsPrice = localStorage.getItem('seniorTicketAmount') * (localStorage.getItem('ticketPrice') / 2);
     totalTicketsPrice = totalBasicTicketsPrice + totalSeniorTicketsPrice;
-
     basicTicketsTotalPrice.textContent = totalBasicTicketsPrice;
     seniorTicketsTotalPrice.textContent = totalSeniorTicketsPrice;
     totalPrice.forEach(item => item.textContent = totalTicketsPrice); 
   }
+  
+  ticketsTypeSelect.addEventListener('change', (e) => {
+    localStorage.setItem('selectedTicketType', e.target.value);
+    payInfo.textContent = localStorage.getItem('selectedTicketType')
+    console.log(e.target);
+    changeTicketTypeSelect();
+    recalcPrice();
+  });
 
   const ticketPlus = (ticketTypeID, ticketTypeFormID, ticketTypeName) => {
     const ticket = document.getElementById(ticketTypeID);
@@ -625,7 +635,6 @@ const calcTotalPrice = () => {
 
     ticket.previousElementSibling.stepUp();
     ticketForm.previousElementSibling.stepUp();
-
     localStorage.setItem(ticketTypeName, ticket.previousElementSibling.value);
     recalcPrice();
   }
@@ -636,25 +645,21 @@ const calcTotalPrice = () => {
 
     ticket.nextElementSibling.stepDown();
     ticketForm.nextElementSibling.stepDown();
-
     localStorage.setItem(ticketTypeName, ticket.nextElementSibling.value);
     recalcPrice();
   }
   
   amountBasicPlus.addEventListener('click', () =>  ticketPlus('amountBasicPlus', 'formAmountBasicPlus', 'basickTicketsAmount'));
   amountSeniorPlus.addEventListener('click', () =>  ticketPlus('amountSeniorPlus', 'formAmountSeniorPlus', 'seniorTicketAmount'));
-
   formAmountBasicPlus.addEventListener('click', () =>  ticketPlus('amountBasicPlus', 'formAmountBasicPlus', 'basickTicketsAmount'));
   formAmountSeniorPlus.addEventListener('click', () =>  ticketPlus('amountSeniorPlus', 'formAmountSeniorPlus', 'seniorTicketAmount'));
-
   amountBasicMinus.addEventListener('click', () =>  ticketMinus('amountBasicMinus', 'formAmountBasicMinus' , 'basickTicketsAmount'));
   amountSeniorMinus.addEventListener('click', () =>  ticketMinus('amountSeniorMinus', 'formAmountSeniorMinus', 'seniorTicketAmount'));
-
   formAmountBasicMinus.addEventListener('click', () =>  ticketMinus('amountBasicMinus', 'formAmountBasicMinus' , 'basickTicketsAmount'));
   formAmountSeniorMinus.addEventListener('click', () =>  ticketMinus('amountSeniorMinus', 'formAmountSeniorMinus', 'seniorTicketAmount'));
 
   recalcPrice();
-  setTicketType();
+  changeTicketTypeSelect();
 }
 
 const setSelectedDate = () => {
