df27f8fcb0abc5e7e2fdcd10cebf1945bf886fb3
Osmolovskii Sergei <osmolovskii_sergei@mail.ru>
2021-12-01 19:46:00 +0300

    init: clone news-JS app

---
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+    constructor() {
+        super('https://newsapi.org/v2/', {
+            apiKey: 'e4f9d42a800b4da29d0f18c70c73006e', // получите свой ключ https://newsapi.org/
+        });
+    }
+}
+
+export default AppLoader;
